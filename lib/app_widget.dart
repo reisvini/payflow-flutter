568863432dc58b190e67f5714712150b751ca5d5
Vinicius Reis <vinicius.moreira2003@gmail.com>
2022-01-28 16:00:50 -0300

    feat: adding barcode_scanner and insert_boleto route

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,22 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/insert_boleto/insert_boleto_page.dart';
 import 'package:payflow/modules/login/login_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
-  const AppWidget({Key? key}) : super(key: key);
+  AppWidget() {
+    // SystemChrome.setPreferredOrientations(
+    //     [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]);
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Pay Flow',
-        theme: ThemeData(primaryColor: AppColors.primary),
+        theme: ThemeData(
+            primarySwatch: Colors.orange, primaryColor: AppColors.primary),
         initialRoute: '/splash',
         routes: {
           '/splash': (context) => SplashPage(),
           '/login': (context) => LoginPage(),
           '/home': (context) => HomePage(),
+          '/barcode_scanner': (context) => BarcodeScannerPage(),
+          '/insert_boleto': (context) => InsertBoletoPage(),
         });
   }
 }
