63293843a2443e5a3a76db1a0781284a54eefe57
Vinicius Reis <vinicius.moreira2003@gmail.com>
2022-01-28 00:54:46 -0300

    feature: login and auth functionality

---
+++ lib/modules/login/login_controller.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:payflow/shared/auth/auth_controller.dart';
+import 'package:payflow/shared/models/user_model.dart';
+
+class LoginController {
+  Future<void> googleSignIn(BuildContext context) async {
+    final authController = AuthController();
+    GoogleSignIn _googleSignIn = GoogleSignIn(
+      scopes: [
+        'email',
+      ],
+    );
+
+    try {
+      final response = await _googleSignIn.signIn();
+      final user =
+          UserModel(name: response!.displayName!, photoURL: response.photoUrl);
+      authController.setUser(context, user);
+    } catch (e) {
+      print(e);
+    }
+  }
+}
