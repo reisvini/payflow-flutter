53a01fc1e698e382accbced1485a6400e93c2565
Vinicius Reis <vinicius.moreira2003@gmail.com>
2022-02-02 11:19:25 -0300

    feat: create button widget

--- lib/shared/widgets/set_label_buttons/set_label_buttons.dart
+++ lib/shared/widgets/set_label_buttons/set_label_buttons.dart
@@ -10,31 +10,48 @@ class SetLabelButton extends StatelessWidget {
   final String secondaryLabel;
   final VoidCallback secondaryOnPressed;
   final bool enablePrimaryColor;
+  final bool enableSecondaryColor;
   const SetLabelButton(
       {Key? key,
       required this.primaryLabel,
       required this.primaryOnPressed,
       required this.secondaryLabel,
       required this.secondaryOnPressed,
-      this.enablePrimaryColor = false})
+      this.enablePrimaryColor = false,
+      this.enableSecondaryColor = false})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: AppColors.shape,
-      height: 56,
-      child: Row(children: [
-        Expanded(
-            child: LabelButton(
-                label: primaryLabel,
-                onPressed: primaryOnPressed,
-                style: enablePrimaryColor ? TextStyles.buttonPrimary : null)),
-        DividerVerticalWidget(),
-        Expanded(
-            child: LabelButton(
-                label: secondaryLabel, onPressed: secondaryOnPressed)),
-      ]),
+      height: 57,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Divider(thickness: 1, height: 1, color: AppColors.stroke),
+          Container(
+            height: 56,
+            child: Row(children: [
+              Expanded(
+                  child: LabelButton(
+                      label: primaryLabel,
+                      onPressed: primaryOnPressed,
+                      style: enablePrimaryColor
+                          ? TextStyles.buttonPrimary
+                          : null)),
+              DividerVerticalWidget(),
+              Expanded(
+                  child: LabelButton(
+                      label: secondaryLabel,
+                      onPressed: secondaryOnPressed,
+                      style: enableSecondaryColor
+                          ? TextStyles.buttonPrimary
+                          : null)),
+            ]),
+          ),
+        ],
+      ),
     );
   }
 }
