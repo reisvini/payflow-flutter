5e923d89563e0631de55623b0813eecbc0932f14
Vinicius Reis <vinicius.moreira2003@gmail.com>
2022-02-02 23:43:23 -0300

    feat: scondary color

--- lib/shared/themes/app_colors.dart
+++ lib/shared/themes/app_colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const primary = Color(0xFFFF941A);
+  static const secondary = Color(0xFF585666);
   static const grey = Color(0xFF585666);
   static const delete = Color(0xFFE83F5B);
   static const heading = Color(0xFF585666);
@@ -9,4 +10,5 @@ class AppColors {
   static const stroke = Color(0xFFE3E3E6);
   static const shape = Color(0xFFFAFAFC);
   static const background = Color(0xFFFFFFFF);
+  static const input = Color(0xFFB1B0B8);
 }
